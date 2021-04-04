f4f1a9fc3797eb198e90bd0da59e707b5151290b
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-04-03 19:35:26 -0700

    update common.mk file

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 SHELL=/bin/bash
 
-ifeq ($(shell echo ${GOLLYX_PELICAN_HOME}),)
-$(error Environment variable GOLLYX_PELICAN_HOME not defined. Please run "source environment" in the repo root directory before running make commands)
+ifeq ($(shell echo ${GOLLYX_PSEUDO_PELICAN_HOME}),)
+$(error Environment variable GOLLYX_PSEUDO_PELICAN_HOME not defined. Please run "source environment" in the repo root directory before running make commands)
 endif
 
 #ifeq ($(shell which jq),)
